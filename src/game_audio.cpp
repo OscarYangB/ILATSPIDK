90e524c842d65351d91a3c1a5ddfb32091f1d0d6
Oscar Yang <oscaryangb@gmail.com>
2026-01-14 20:59:37 -0500

    Set up some more audio stuff

--- src/game_audio.cpp
+++ src/game_audio.cpp
@@ -1,33 +1,46 @@
 #include "game_audio.h"
 #include "SDL3/SDL_audio.h"
 #include "SDL3/SDL_iostream.h"
+#include "SDL3/SDL_stdinc.h"
 #include "game_assets.h"
-#include "definitions.h"
+#include <algorithm>
 
 SDL_AudioStream* audio_stream = nullptr;
+std::vector<PlayingAudio> playing_audio;
 
 void init_audio() {
+	SDL_AudioSpec spec;
+	spec.channels = 2;
+	spec.format = SDL_AUDIO_S32LE; // All played audio needs to be in this format
+	spec.freq = 44100;
+	audio_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, audio_stream_callback, nullptr);
+	SDL_ResumeAudioStreamDevice(audio_stream);
 }
 
 void audio_stream_callback(void* userdata, SDL_AudioStream* stream, int additional_amount, int total_amount) {
+	for (int i = 0; i < playing_audio.size(); i++) {
+		u32 samples_to_play = SDL_min(total_amount, playing_audio[i].remaining_samples());
+		SDL_PutAudioStreamData(stream, playing_audio[i].data + playing_audio[i].position, samples_to_play);
+		playing_audio[i].position += samples_to_play;
+	}
 
+	auto remove_if = std::remove_if(playing_audio.begin(), playing_audio.end(), [](PlayingAudio audio) {return audio.position == audio.length;});
+	for (auto iter = remove_if; iter != playing_audio.end(); iter++) {
+		SDL_free(iter->data);
+	}
+	playing_audio.erase(remove_if, playing_audio.end());
 }
 
+// Could do reference counted loading. Maybe a component that keeps a file in memory if an entity needs it during its lifetime.
+// Definitely should not load file if it is already loaded. Should just read from memory.
 void play_audio(AudioAsset audio_asset) {
-	SDL_AudioSpec spec;
-
 	u8* data;
 	u32 length;
 
 	u8 audio_index = static_cast<u8>(audio_asset);
 	SDL_IOStream* input_stream = SDL_IOFromConstMem(audio_data[audio_index], audio_sizes[audio_index]);
+	SDL_AudioSpec spec;
 	SDL_LoadWAV_IO(input_stream, true, &spec, &data, &length);
 
-	audio_stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, nullptr, nullptr);
-
-	SDL_free(data);
-
-	SDL_PutAudioStreamData(audio_stream, data, length);
-
-	SDL_ResumeAudioStreamDevice(audio_stream);
+	playing_audio.push_back(PlayingAudio{data, length, 0});
 }
--- src/game_audio.h
+++ src/game_audio.h
@@ -1,9 +1,21 @@
 #pragma once
+#include "definitions.h"
+#include <vector>
 
 class SDL_AudioStream;
 enum class AudioAsset;
 
+struct PlayingAudio {
+	u8* data;
+	u32 length;
+	u32 position;
+
+	inline u32 remaining_samples() { return length - position; }
+};
+
 extern SDL_AudioStream* audio_stream;
+extern std::vector<PlayingAudio> playing_audio;
 
 void init_audio();
+void audio_stream_callback(void* userdata, SDL_AudioStream* stream, int additional_amount, int total_amount);
 void play_audio(AudioAsset audio_asset);
