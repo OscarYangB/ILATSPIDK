98f4c814e839b54f69d0cfa013dcaeac759af3d5
Oscar Yang <oscaryangb@gmail.com>
2026-01-20 16:56:16 +0800

    Atlas import/export tooling

---
+++ tools/export_aseprite.py
@@ -0,0 +1,19 @@
+import subprocess
+
+aseprite_path = "C:/Program Files/Aseprite/Aseprite.exe"
+print("name: ")
+name = input()
+subprocess.run([aseprite_path,
+                "--sheet", "C:/Projects/ILATSPIDK/assets/atlas/%s.png"%name,
+                "--data", "C:/Projects/ILATSPIDK/assets/atlas/%s.json"%name,
+                "--sheet-pack",
+                "--split-layers",
+                "--all-layers",
+                "--ignore-empty",
+                "--format", "json-array",
+                "--list-layer-hierarchy",
+                "--batch",
+                "--filename-format",
+                "{title}_{group}_{layer}_{frame1}",
+                "--ignore-layer", "Reference",
+                "C:/Projects/Aseprite Projects/%s.aseprite"%name], shell=True)
