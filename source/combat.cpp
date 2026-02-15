81cb9340f115a1ff43c4d9813b5b876cd6fdfc94
Oscar Yang <oscaryangb@gmail.com>
2026-02-15 17:56:39 +0800

    Sketch out some combat code

---
+++ source/combat.cpp
@@ -0,0 +1,99 @@
+#include "combat.h"
+#include <algorithm>
+#include "game.h"
+
+Combat combat{};
+bool in_combat = false;
+
+bool PlayedCard::is_valid() {
+	return card != nullptr;
+}
+
+void PlayedCard::invalidate() {
+	bars_until_activate = 0;
+	card = nullptr;
+	targets.clear();
+}
+
+void CharacterComponent::init_from_data(const CharacterDataComponent& new_data) {
+	data = &new_data;
+	health = data->starting_health;
+	max_health = data->starting_health;
+	shield = data->starting_shield;
+	hand = {};
+	status_effects = {};
+	played_card = {};
+}
+
+void CharacterComponent::heal(float amount) {
+	health += amount;
+	health = std::clamp(health, 0.f, max_health);
+}
+
+void CharacterComponent::damage(float amount) {
+	float damage_to_shield = std::min(shield, amount);
+	shield -= damage_to_shield;
+	health -= amount - damage_to_shield;
+	health = std::clamp(health, 0.f, max_health);
+}
+
+void CharacterComponent::draw() {
+	if (deck.empty()) {
+		return;
+	}
+
+	Card card = deck.front();
+	deck.pop_back();
+	hand.push_back(card);
+}
+
+void CharacterComponent::play_card(u8 hand_index, const std::vector<CharacterComponent>& targets) {
+	Card card = hand[hand_index];
+	hand.erase(hand.begin() + hand_index);
+	played_card.card = card;
+	played_card.bars_until_activate = card->cost;
+	played_card.targets = targets;
+	card->play(*this, targets);
+}
+
+void CharacterComponent::on_bar_end() {
+	if (!played_card.is_valid()) {
+		return;
+	}
+
+	played_card.bars_until_activate--;
+
+	if (played_card.bars_until_activate <= 0) {
+		played_card.card->activate(*this, played_card.targets);
+		played_card.invalidate();
+	}
+}
+
+void CharacterComponent::on_turn_start() {
+	draw();
+}
+
+void start_combat(const Characters& characters) {
+	in_combat = true;
+	combat = {characters, 0};
+}
+
+void Combat::update() {
+	timer += delta_time;
+
+	if (timer >= SECONDS_PER_BAR) { // Bar ends
+		timer -= SECONDS_PER_BAR;
+		bar_index++;
+		characters[turn_index].on_bar_end();
+
+		if (bar_index >= BARS_PER_TURN) { // Turn ends
+			bar_index = 0;
+			turn_index++;
+			characters[turn_index].on_turn_start();
+
+			if (turn_index >= characters.size()) { // Cycle ends
+				turn_index = 0;
+			}
+		}
+	}
+}
---
+++ source/combat.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <vector>
+#include "definitions.h"
+#include "image_assets.h"
+
+struct CharacterComponent;
+using Characters = std::vector<CharacterComponent>;
+
+struct CardData {
+	const char* name;
+	const char* description;
+	const char* play_text;
+	u8 cost;
+	u8 minigame_level; // 0 means none
+	u8 enemy_target_bitmask;
+	u8 number_of_targets;
+
+	void (*play)(CharacterComponent& character, const Characters& targets);
+	void (*activate)(CharacterComponent& character, const Characters& targets);
+};
+
+using Card = const CardData*;
+
+struct PlayedCard {
+	u8 bars_until_activate;
+	Card card = nullptr;
+	Characters targets = {};
+
+	bool is_valid();
+	void invalidate();
+};
+
+enum CharacterType : u8 {
+	GOOD = 1 << 0,
+	EVIL = 1 << 1,
+	FURNITURE = 1 << 2,
+};
+
+struct StatusEffect {
+	float seconds_timer;
+	u8 bar_timer;
+	u8 turn_timer;
+	AtlasIndex icon;
+
+	void (*on_add)();
+	/* void update(); // TODO */
+	/* void on_bar_end(); // TODO */
+	/* void on_turn_end(); // TODO */
+	void (*on_remove)();
+};
+
+struct CharacterDataComponent {
+	float starting_health;
+	float starting_shield;
+	CharacterType type;
+	std::vector<Card> starting_deck;
+	AtlasIndex icon;
+};
+
+struct CharacterComponent {
+	const CharacterDataComponent* data;
+	float health;
+	float max_health;
+	float shield;
+	std::vector<Card> deck;
+	std::vector<Card> hand;
+	std::vector<StatusEffect> status_effects;
+	PlayedCard played_card;
+
+	void init_from_data(const CharacterDataComponent& new_data);
+	inline bool is_alive() { return health > 0.f; }
+	void heal(float amount);
+	void damage(float amount);
+	void draw();
+	void play_card(u8 hand_index, const Characters& targets);
+	void on_bar_end();
+	void on_turn_start();
+};
+
+constexpr double BPM = 135.0;
+constexpr u8 BEATS_PER_BAR = 4;
+constexpr double BEATS_PER_SECOND = BPM / 60.0;
+constexpr double SECONDS_PER_BEAT = 1.0 / BEATS_PER_SECOND;
+constexpr double SECONDS_PER_BAR = SECONDS_PER_BEAT * BEATS_PER_BAR;
+constexpr double BARS_PER_TURN = 4;
+
+struct Combat {
+	Characters characters{};
+	u8 turn_index = 0;
+	double timer = 0.f;
+	u8 bar_index = 0;
+
+	void update();
+};
+
+extern Combat combat;
+extern bool in_combat;
+
+void start_combat(const Characters& characters);
+void update_combat();
+
+constexpr CardData card_data[] {
+	{"Fireball", "Shoot ball of fire", "is shooting a fireball!", 3,
+	 0, GOOD, 1,
+	 [](CharacterComponent& character, const Characters& targets){
+
+	 },
+	 [](CharacterComponent& character, const Characters& targets){
+
+	 }},
+};
