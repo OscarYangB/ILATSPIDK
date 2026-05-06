8639103f9d260b193ca6e2cdec783462a8f764ae
oscaryangb <oscaryangb@gmail.com>
2026-05-07 01:19:54 +0800

    Export sprites to Godot for level editor preview

--- source/position_data.h
+++ source/position_data.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "vector2.h"
 
-constexpr Vector2 PLAYER_POSITION = {499, -472};
 constexpr Vector2 ENEMY_POSITION = {1521, -515};
+constexpr Vector2 PLAYER_POSITION = {424, -188};
--- tools/export_aseprite.py
+++ tools/export_aseprite.py
@@ -1,6 +1,8 @@
 import subprocess
 
 aseprite_path = "C:/Program Files/Aseprite/Aseprite.exe"
+godot_project_path = "C:/Projects/Godot Projects/level-editor/"
+
 print("name:", end=" ")
 name = input()
 subprocess.run([aseprite_path,
@@ -16,4 +18,12 @@ subprocess.run([aseprite_path,
                 "--filename-format",
                 "{title}_{group}_{layer}_{frame1}",
                 "--ignore-layer", "Reference",
+                "C:/Projects/Aseprite Projects/%s.aseprite"%name], shell=True)
+subprocess.run([aseprite_path,
+                "--sheet", godot_project_path + "%s.png"%name,
+                "--data", godot_project_path + "image_json_data/%s.json"%name,
+                "--all-layers",
+                "--ignore-empty",
+                "--batch",
+                "--oneframe",
                 "C:/Projects/Aseprite Projects/%s.aseprite"%name], shell=True)
